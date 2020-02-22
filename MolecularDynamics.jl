8b1de01353d5b51523d0a85ce83d7b3dd9daa0e5
yutaka <p7dea87c7e4n7j@gmail.com>
2020-02-22 22:41:06 +0900

    feat: lennard jones and excluded box wall potential

--- MolecularDynamics.jl
+++ MolecularDynamics.jl
@@ -1,3 +1,4 @@
+using Printf
 using Random
 using Distributions
 using LinearAlgebra
@@ -7,12 +8,15 @@ const time_step = 0.01
 const total_step = 300
 const dump_step = 1
 const lennard_jones_eps = 0.6
+const lennard_jones_sigma = 4.0
 const initial_position_range = (-10.0 ,10.0)
 const initial_velocity_range = (-10.0 ,10.0)
 const mass_range             = (100.0   ,101.0)
-const box_side_length        = 30.0
+const box_side_length        = 25.0
 const box_eps                = 0.6
-const box_sigma              = 0.5
+const box_sigma              = 4.0
+
+log_file = open("logfile.log", "w")
 
 function lennard_jones(eps::Float64, sig::Float64, r::Float64)::Float64
     sig_r_6 = (sig / r)^6
@@ -27,9 +31,39 @@ function dev_lennard_jones(eps::Float64, sig::Float64, r::Float64)::Float64
     24.0 * eps * rinv * (sig_r_6 - 2.0 * sig_r_12)
 end
 
+function excluded_volume(eps::Float64, sig::Float64, r::Float64)::Float64
+    eps * (sig / r)^12
+end
+
 function dev_excluded_volume(eps::Float64, sig::Float64, r::Float64)::Float64
     rinv = 1.0 / r
     -12.0 * eps * rinv * (sig * rinv)^12
+end
+
+function calculate_energy(coord_vec::Array{Float64, 2}, velocity_vec::Array{Float64, 2},
+                          mass_vec::Array{Float64, 2})::Float64
+    total_energy = 0
+    # lennard jones part
+    for i in 1:particle_num - 1
+        for j in i + 1:particle_num
+            distance = norm(coord_vec[:,i] - coord_vec[:,j])
+            println(log_file, "distance between $(i), $(j) ", distance)
+            total_energy += lennard_jones(lennard_jones_eps, lennard_jones_sigma, distance)
+        end
+    end
+
+    println(log_file, "lennard jones energy ", total_energy)
+
+    # box potential part
+    box_side_coord = box_side_length * 0.5
+    total_energy +=
+        sum(coord -> excluded_volume(box_eps, box_sigma, box_side_coord - abs(coord)), coord_vec)
+    println(log_file, "total energy ", total_energy)
+
+    # physical energy
+    total_energy += sum(0.5 * velocity_vec .* velocity_vec .* mass_vec)
+
+    total_energy
 end
 
 function calculate_force(coord_vec::Array{Float64, 2})::Array{Float64, 2}
@@ -39,19 +73,23 @@ function calculate_force(coord_vec::Array{Float64, 2})::Array{Float64, 2}
         for j in i+1:particle_num
             dist_vec = coord_vec[:,j] - coord_vec[:,i]
             distance = norm(dist_vec)
-            lennard_force_vec = dev_lennard_jones(lennard_jones_eps, 0.1, distance) * dist_vec / distance
+            lennard_force_vec = dev_lennard_jones(lennard_jones_eps, lennard_jones_sigma, distance) * dist_vec / distance
             res_force_vec[:,i] += lennard_force_vec
             res_force_vec[:,j] -= lennard_force_vec
         end
     end
+
+    #println(log_file, "lennard jones force ", res_force_vec)
 
     # box potential part
     box_side_coord = box_side_length * 0.5
     box_force_vec = map(coord_vec) do coord
         dev_excluded_volume(box_eps, box_sigma, box_side_coord - abs(coord)) * sign(coord)
     end
+    #println(log_file, "box force ", box_force_vec)
     res_force_vec += box_force_vec
 
+    #println(log_file, "total force ", res_force_vec)
     res_force_vec
 end
 
@@ -65,6 +103,7 @@ function main()
     # pre preparation
     inv_mass_vec = 1 ./ mass_vec
     frame_vec_for_dump = []
+    energy_vec_for_dump = []
     push!(frame_vec_for_dump, coord_vec)
 
     # system integration
@@ -73,13 +112,15 @@ function main()
     for step_idx in 1:total_step
         coord_vec = coord_vec + time_step * velocity_vec + time_step * time_step * force_vec .* inv_mass_vec * 0.5
         new_force_vec = calculate_force(coord_vec)
-        velocity_vec = velocity_vec + time_step * (force_vec + new_force_vec) * 0.5
+        velocity_vec = velocity_vec + time_step * (force_vec + new_force_vec) .* inv_mass_vec * 0.5
 
         force_vec = new_force_vec
 
+        # for dump
         if mod(step_idx, dump_step) == 0
             println("step ", step_idx)
             push!(frame_vec_for_dump, coord_vec)
+            push!(energy_vec_for_dump, calculate_energy(coord_vec, velocity_vec, mass_vec))
         end
     end
 
@@ -89,10 +130,17 @@ function main()
             println(os, particle_num)
             println(os, "step = ", idx * dump_step)
             for (idx, atom) in enumerate(eachcol(frame))
-                println(os,"Particle" ,idx ," " , atom[1], " ", atom[2], " ", atom[3])
+                @printf(os, "Particle%d %11.8f %11.8f %11.8f\n", idx, atom[1], atom[2], atom[3])
             end
+        end
+    end
+
+    open("energy.dat", "w") do os
+        for energy in energy_vec_for_dump
+            println(os, energy)
         end
     end
 end
 
 main()
+close(log_file)
