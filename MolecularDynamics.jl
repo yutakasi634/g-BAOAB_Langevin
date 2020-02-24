d1670f9c2315bdde233789f76eeb2c1d5575df0d
yutaka <p7dea87c7e4n7j@gmail.com>
2020-02-24 15:30:20 +0900

    implement BAOAB langevin

--- MolecularDynamics.jl
+++ MolecularDynamics.jl
@@ -6,7 +6,10 @@ using LinearAlgebra
 # simulation meta information
 const time_step = 0.1
 const total_step = 100000
-const dump_step = 10
+const dump_step = 100
+
+# physical constants
+const kB = 0.0019872 # kcal/ mol K
 
 # system paraneters
 const particle_num  = 5
@@ -28,11 +31,13 @@ const rng = MersenneTwister(1234)
 
 # pre calculation
 const time_step2 = time_step * time_step
-const kB = 0.0019872 # kcal/ mol K
 const one_minus_gammah_over2 = 1 - gamma * time_step * 0.5
 const mass_vec     = rand(Uniform(mass_range...), 1, particle_num)
 const inv_mass_vec = 1 ./ mass_vec
+const sqrt_inv_mass_vec = sqrt.(inv_mass_vec)
 const noise_coef_vec = sqrt.(2gamma * kB * temperature / time_step .* inv_mass_vec)
+const baoab_c_1 = ℯ^(-gamma * time_step)
+const baoab_c_3 = sqrt(kB * temperature * (1 - baoab_c_1^2))
 
 log_file = open("logfile.log", "w")
 
@@ -112,7 +117,7 @@ function calculate_force(coord_vec::Array{Float64, 2})::Array{Float64, 2}
 end
 
 function velocity_verlet_integration(coord_vec::Array{Float64, 2}, vel_vec::Array{Float64, 2},
-                                     acceleration_vec::Array{Float64, 2}, inv_mass_vec::Array{Float64, 2})::Tuple{Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}}
+                                     acceleration_vec::Array{Float64, 2})::Tuple{Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}}
     new_coord_vec =
         coord_vec + time_step * vel_vec + time_step2 * 0.5 * acceleration_vec
     new_acceleration_vec = calculate_force(new_coord_vec) .* inv_mass_vec
@@ -122,7 +127,7 @@ function velocity_verlet_integration(coord_vec::Array{Float64, 2}, vel_vec::Arra
 end
 
 function langevin_integration(coord_vec::Array{Float64, 2}, vel_vec::Array{Float64, 2},
-                              acceleration_vec::Array{Float64, 2}, inv_mass_vec::Array{Float64, 2})::Tuple{Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}}
+                              acceleration_vec::Array{Float64, 2})::Tuple{Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}}
     new_coord_vec =
         coord_vec + vel_vec * time_step * one_minus_gammah_over2 +
         time_step2 * 0.5 * acceleration_vec
@@ -132,6 +137,18 @@ function langevin_integration(coord_vec::Array{Float64, 2}, vel_vec::Array{Float
     new_vel_vec =
         one_minus_gammah_over2 * (one_minus_gammah_over2 + (gamma * time_step * 0.5)^2) .* vel_vec +
         0.5 * time_step * one_minus_gammah_over2 * (acceleration_vec + new_acceleration_vec)
+    (new_coord_vec, new_vel_vec, new_acceleration_vec)
+end
+
+function BAOAB_langevin_integration(coord_vec::Array{Float64, 2}, vel_vec::Array{Float64, 2},
+                                    acceleration_vec::Array{Float64, 2})::Tuple{Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}}
+    temp_vel_vec         = vel_vec + time_step * 0.5 .* acceleration_vec
+    temp_coord_vec       = coord_vec + time_step * 0.5 .* temp_vel_vec
+    temp_hat_vel_vec     =
+        baoab_c_1 .* temp_vel_vec + baoab_c_3 .* sqrt_inv_mass_vec .* randn(rng, (3, particle_num))
+    new_coord_vec        = temp_coord_vec + time_step * 0.5 .* temp_hat_vel_vec
+    new_acceleration_vec = calculate_force(new_coord_vec) .* inv_mass_vec
+    new_vel_vec          = temp_hat_vel_vec + time_step * 0.5 .* new_acceleration_vec
     (new_coord_vec, new_vel_vec, new_acceleration_vec)
 end
 
@@ -152,10 +169,10 @@ function main()
     acceleration_vec = calculate_force(coord_vec) .* inv_mass_vec
     # loop start
     for step_idx in 1:total_step
-
         coord_vec, velocity_vec, acceleration_vec =
-            #velocity_verlet_integration(coord_vec, velocity_vec, acceleration_vec, inv_mass_vec)
-            langevin_integration(coord_vec, velocity_vec, acceleration_vec, inv_mass_vec)
+            #velocity_verlet_integration(coord_vec, velocity_vec, acceleration_vec)
+            #langevin_integration(coord_vec, velocity_vec, acceleration_vec)
+            BAOAB_langevin_integration(coord_vec, velocity_vec, acceleration_vec)
 
         # for dump
         if mod(step_idx, dump_step) == 0
