c20ab7074ab72ee4f487f731162bfaf40bd61d86
yutaka <p7dea87c7e4n7j@gmail.com>
2020-03-04 21:24:33 +0900

    update visualization tcl script

--- visualize.tcl
+++ visualize.tcl
@@ -1,9 +1,11 @@
 display depthcue off
 mol addrep 0
 mol modselect 0 0 name CORE
-mol modstyle 0 0 VDW 2.4 15.0
+mol modstyle 0 0 VDW 3.3 15.0
 mol modcolor 0 0 ColorID 8
+mol modmaterial 0 0 Diffuse
 mol addrep 0
 mol modselect 1 0 name PATCH
 mol modstyle 1 0 VDW 0.8 15.0
-mol modcolor 1 0 ColorID 30
+mol modcolor 1 0 ColorID 23
+mol modmaterial 1 0 Diffuse
